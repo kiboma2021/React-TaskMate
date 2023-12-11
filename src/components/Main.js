7fcf910e3e4eee728a3c55e1ff29f3d377cefa7b
kiboma <omayioben@gmail.com>
2023-12-11 19:48:27 +0300

    Add and style task section

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,6 +6,24 @@ const Main = () => {
             <input type="text" placeholder="Task" />
             <button type="submit" className="addBtn">Save</button>
         </div>
+        <div className="task_section">
+            <div className="task_header">
+                <h2>Todo <span> </span></h2>
+                <button>Clear All</button>
+            </div>
+            <hr />
+            <div className="task_plate">
+                <div className="task_name">
+                    <h3>Task Name</h3>
+                    <p>Time and Date</p>                    
+                </div>
+                <div className="task_actions">
+                    <span>Edit</span>
+                    <span>Delete</span>
+                </div>
+
+            </div>
+        </div>
       
     </div>
   )
