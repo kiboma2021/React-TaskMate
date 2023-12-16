5464e6d5fccb9eaa1482bd79f3352655623f5187
kiboma <omayioben@gmail.com>
2023-12-16 16:34:01 +0300

    Change theme color

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import Logo from '../assets/logo.jpeg'
 
 const Header = () => {
     
-  const [theme, setTheme] = useState(JSON.parse(localStorage.getItem("theme")) || "light");
+  const [theme, setTheme] = useState("light");
 
   useEffect(() =>{
     document.documentElement.removeAttribute("class");
@@ -17,12 +17,12 @@ const Header = () => {
             <h2>Taskmate</h2>
         </div>
         <div className='color-btns'>
-            <button onClick={()=>setTheme("light")} className="circle light"></button>
-            <button onClick={()=>setTheme("grey")} className="circle grey"></button>
-            <button onClick={()=>setTheme("dark")} className="circle dark activeTheme"></button>
-            <button onClick={()=>setTheme("blue")} className="circle blue"></button>
-            <button onClick={()=>setTheme("green")} className="circle green"></button>
-            <button onClick={()=>setTheme("yellow")} className="circle yellow"></button>
+            <button onClick={()=>setTheme("light")} className={theme === 'light'?'circle light activeTheme':'circle light'}></button>
+            <button onClick={()=>setTheme("grey")} className={theme === 'grey'?'circle grey activeTheme':'circle grey'}></button>
+            <button onClick={()=>setTheme("dark")} className={theme === 'dark'?'circle dark activeTheme':'circle dark'}></button>
+            <button onClick={()=>setTheme("blue")} className={theme === 'blue'?'circle blue activeTheme':'circle blue'}></button>
+            <button onClick={()=>setTheme("green")} className={theme === 'green'?'circle green activeTheme':'circle green'}></button>
+            <button onClick={()=>setTheme("yellow")} className={theme === 'yellow'?'circle yellow activeTheme':'circle yellow'}></button>
         </div>
       
     </div>
