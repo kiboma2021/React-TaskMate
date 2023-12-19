a16c6130d91ad5b776a084c1bdede89ed01b06ef
kiboma <omayioben@gmail.com>
2023-12-19 08:51:07 +0300

    Add route for home and contact page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { useState,useEffect } from 'react';
+import { Routes,Route } from 'react-router-dom';
 import Header from './components/Header';
 import Main from './components/Main';
+import Contact from './components/Contact';
 import './App.css';
 
 function App() {
@@ -9,7 +11,12 @@ function App() {
     <div className="App">
       <div className='components'>
         <Header />
-        <Main />
+        <Routes>
+          <Route path='/' element={<Main />}></Route>
+          <Route path='contact' element={<Contact />}></Route>
+        </Routes>
+
+
       </div>
 
 
