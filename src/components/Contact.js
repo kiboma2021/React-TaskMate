daf10ec5fc1ca166d67e2f623c3afd4edc2c7d11
kiboma <omayioben@gmail.com>
2023-12-19 12:20:31 +0300

    useNavigate

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,23 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const Contact = () => {
+
+  const navigate = useNavigate();
+
+  const handleSubmit = () => {
+    console.log('....')
+    navigate("/")
+  };
   return (
     <div>
 
         <h2>Contact us</h2>
+        <form className='contact' onClick={handleSubmit}>
+          <input type="email" placeholder='Email' maxLength={50} />
+          <input className='issue-desc' type="text" placeholder='Issue description' maxLength={300} />
+          <button type='submit' className='contact-btn'>Submit</button>
+        </form>
       
     </div>
   )
